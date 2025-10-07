4980cf1b3fa3d4ce0153b671afdb95a4305d67d5
ngocsangyem <nnsang24@gmail.com>
2025-10-07 19:36:42 +0700

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import path from 'node:path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    tailwindcss(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+       '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
